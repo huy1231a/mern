913302bacce3bb3dd36e62508367601ec85091c6
huy1231a <taolahuy2k0@gmail.com>
2024-01-11 17:31:03 +0700

    add layout

--- fe/app/UI/Home/Homepage.tsx
+++ fe/app/UI/Home/Homepage.tsx
@@ -1,12 +1,14 @@
-import React from "react";
-import Header from "./header/header";
+import React from 'react'
+import Header from './header/header'
+import Sale from './sale/sale'
 
 const Homepage = () => {
   return (
     <div>
       <Header />
+      <Sale />
     </div>
-  );
-};
+  )
+}
 
-export default Homepage;
+export default Homepage
--- fe/app/UI/Home/sale/sale.tsx
+++ fe/app/UI/Home/sale/sale.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React from 'react'
 
 const Sale = () => {
-  return <div>Sale</div>;
-};
+  return (
+    <div className='flex justify-center mt-5'>
+      <div className='w-[1528px] h-[600px]'>
+        <div className='flex justify-between items-center'>
+          <span className='text-[32px] font-bold leading-normal'>
+            Up to -40% 🎊 Order.uk exclusive deals
+          </span>
+          <div className='flex gap-4 px-2 py-3 cursor-pointer'>
+            <span className='p-5'>Vegan</span>
+            <span className='p-5'>Sushi</span>
+            <div className='w-[219px] h-[61px] border border-[#FC8A06] rounded-full flex justify-center '>
+              <span className='flex justify-center items-center text-[#FC8A06]'>
+                Pizza & Fast food
+              </span>
+            </div>
+            <span className='p-5'>Vegan</span>
+            <span className='p-5'>Others</span>
+          </div>
+        </div>
+        <div className='flex'>
+          <div className='w-[]'>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
-export default Sale;
+export default Sale
